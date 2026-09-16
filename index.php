03e26a73f66dc3349e4de753b966d66a623075bf
Ernest Makarov <ernest.makarov@tptlive.ee>
2026-09-16 10:24:56 +0300

    asd

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
-<h1>
 <?php
-    echo "Hello, World!";
-    echo "This is a simple PHP script.";
+    function Hello() {
+        var_dump('Hello!');
+    }
+
+    Hello();
+    Hello();
+
+    function square($a) {
+        if ($a<0){
+            echo 'Error: Negative number';
+        }
+        return $a * $a;
+        var_dump('asdasdads');
+    }
+ 
+    $answer = square(4);
+    var_dump($answer);
+    var_dump(square(5));
+
+    for ($i=0; $i<10; $i++) {
+        var_dump($i);
+    }
 ?>
-</h1>
